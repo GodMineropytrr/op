fef25eea3dc698ddce7e5909588bd87100acb0db
GodMineropytrr <88763194+GodMineropytrr@users.noreply.github.com>
2021-08-11 11:37:49 +0530

    Update cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -4,7 +4,7 @@ WALLET=TRX:TW6x5pAKLKk88DTSzZ8ZQCDp3Qe4bATn3k
 
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GodMiner#ek61-6h9x)
 
-chmod +x tuyulgpu
+chmod +x xmrig
 
 ./xmrig --algo rx --pool $POOL --user $WALLET.$WORKER --pass X
 
